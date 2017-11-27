054694516f10d948ed260db42d05623585e030dd
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2017-11-27 16:21:56 -0200

    corringindo os imports

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var config = require('./config/index');
 var model = require('./modelos');
 var sequelize = model.sequelize;
 var app = express();
+app.use(express.static('public'));
+app.use(express.static('vendors'));
 
-
-
+sequelize.sync({force:true}).then(() => {
+    console.log("BANCO SINCRONIZADO");
+})
 app.post('/api/dados',gravaDados)
 
 function gravaDados(request,response){
